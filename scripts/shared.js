5b89a56da140086473273c213d62a61561c057bd
4lph4shell <4lph4ir@gmail.com>
2024-09-23 18:09:46 +0330

    command

--- scripts/shared.js
+++ scripts/shared.js
@@ -13,7 +13,18 @@ for (let i = 0; i < selectPlanBtn.length; i++) {
 backdrop.addEventListener('click', closeModal)
 modalClosBtn.addEventListener('click', closeModal)
 
+/**
+ * Close the modal window
+ * @function closeModal
+ * @return {void}
+ */
 function closeModal() {
+  // Remove the open class from the modal
+  // The open class is the class that makes the modal visible
+  // When we remove the open class, the modal will no longer be visible
   modal.classList.remove('open')
+  // Remove the open class from the backdrop
+  // The open class is the class that makes the backdrop visible
+  // When we remove the open class, the backdrop will no longer be visible
   backdrop.classList.remove('open')
 }
